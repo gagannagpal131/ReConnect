7ff7fe13ad4791a2b5f274f8b662dfce4e0f07fe
Sehajdeep Singh <sehajdeep33@gmail.com>
2019-10-07 18:27:07 -0400

    Extracting datatype of variable, storing datatype and variables in an array | For int

---
+++ regex.py
@@ -0,0 +1,53 @@
+import re
+
+str = "int a=100,b=9,c;"
+
+# Check if "int" is present at the beginning of a word:
+
+datatype = re.findall("^int", str)
+
+array = [datatype[0]]
+
+print("Datatype =", array[0])
+
+##### When declaration is like "int a;"
+# x = re.split("\s|;", str)
+# x.remove("int")
+# for i in x:
+#     x.remove("")
+# array.append(x)
+# print("Variables=", array[1])
+
+##### When declaration is like "int a,b;"
+# x = re.split("\s|;|,", str)
+# x.remove("int")
+# for i in x:
+#     x.remove("")
+#
+# for i in range(len(x)):
+#     if x[i] != "":
+#         array.append(x[i])
+#
+# print("Variables =")
+# for i in range(1, len(array)):
+#     print(array[i])
+
+##### When declaration is like "int a=10,b=10,d,d;"
+x = re.split("\s|;|,|=|[0-9]", str)
+x.remove("int")
+
+for i in x:
+    x.remove("")
+
+print(x)
+# x = [re.sub([0-9], '', i) for i in x]
+
+for i in range(len(x)):
+    if x[i] != "":
+        array.append(x[i])
+
+print("Variables =")
+for i in range(1, len(array)):
+    print(array[i])
+
+print(array)
