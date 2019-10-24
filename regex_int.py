9a925cf04ac37d78e365efa59f5f8b292439eba3
Gagandeep Nagpal <>
2019-10-24 09:06:02 -0400

    Addition of Information in Code

--- regex_int.py
+++ regex_int.py
@@ -1,5 +1,5 @@
-import re
 
+import re
 str = "int a=100,b=9,c;"
 
 # Check if "int" is present at the beginning of a word:
@@ -11,6 +11,7 @@ array = [datatype[0]]
 print("Datatype =", array[0])
 
 ##### When declaration is like "int a=10,b=10,d,d;"
+#There can be space between the strings.
 x = re.split("\s|;|,|=|[0-9]", str)
 x.remove("int")
 
