5d4791607285ad3ad82c4e2494a03106f9f2036b
Gagandeep Nagpal <>
2019-11-08 18:29:50 -0500

    Dictionaries Successfully imported from logic file.

--- UI.py
+++ UI.py
@@ -1,6 +1,18 @@
 #This file is to implement the UI for the logic.
+import logic
 from tkinter import *
 
+classList, dictClassInheritances,dictClassMethods,dictClassVariables = logic.returnToUI()
+print(classList)
+print()
+print(dictClassInheritances)
+print()
+print(dictClassMethods)
+print()
+print(dictClassVariables)
+print()
+
+"""
 window = Tk()
 my_var = " hello"
 window.title("Welcome to ReConnect: Class Level Architecture Recovery")
@@ -10,3 +22,4 @@ lbl = Label(window, text="Hello"+my_var)
 lbl.grid(column=300, row=300)
 
 window.mainloop()
+"""
