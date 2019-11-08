60d9ee18ecae0089bc617502d582c9d0f1540b50
Gagandeep Nagpal <>
2019-11-08 16:45:11 -0500

    Frame Work for UI Imported

--- UI.py
+++ UI.py
@@ -1,1 +1,12 @@
 #This file is to implement the UI for the logic.
+from tkinter import *
+
+window = Tk()
+my_var = " hello"
+window.title("Welcome to ReConnect: Class Level Architecture Recovery")
+
+lbl = Label(window, text="Hello"+my_var)
+
+lbl.grid(column=300, row=300)
+
+window.mainloop()
