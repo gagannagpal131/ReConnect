8af1b1fad5e59658231a7bbcae3a9d3845df2315
gagannagpal131 <gagannagpal131@gmail.com>
2019-11-17 13:36:56 -0500

    UI Updated and code checks key in dictionary before displaying the key,value in dictionary.

--- UI.py
+++ UI.py
@@ -17,36 +17,50 @@ def printClassDetails(className):
     #Creating new window
     windowNew = tk.Tk()
     windowNew.title(className)
-    windowNew.geometry('500x350')
+    #windowStart = 500+(counter*300)
+    windowStart = 500;
+    windowNew.geometry('400x600+%d+%d'%(windowStart,windowStart))
     windowNew['bg'] = 'LightBlue1'
 
-    #Adding inheritences 
-    st1=dictClassInheritances[className]
-    Section1 = tk.Text(windowNew, height=6, width=30)
-    Section1.pack()    
+    #Adding inheritences
+    Section1 = tk.Text(windowNew, height=10, width=40)
+    Section1.pack(pady = 20)
+    if className in dictClassInheritances:
+        Section1.insert(tk.END, 'Format: <Access Specifier> <Class Name>\n\n')
+        st1=dictClassInheritances[className]
+        for i in st1:
+            Section1.insert(tk.END, i+'\n')
+    else:
+        Section1.insert(tk.END, 'No Classes are Inherited by this class\n')
     #w = tk.Label(Section1, text=dictClassInheritances[st1])
     #w.pack()
-    for i in st1:
-       Section1.insert(tk.END, i+'\n')
 
     #Adding methods
-    st2=dictClassMethods[className]
-    Section2 = tk.Text(windowNew, height=6, width=30)
-    Section2.pack()
-    for i in st2:
-        Section2.insert(tk.END, i+'\n')
-       
+    Section2 = tk.Text(windowNew, height=10, width=40)
+    Section2.pack(pady = 20)
+    if className in dictClassMethods:
+        Section2.insert(tk.END, 'Format: <Return Type> <Method Name>\n\n')
+        st2=dictClassMethods[className]
+        for i in st2:
+            Section2.insert(tk.END, i+'\n')
+    else:
+        Section2.insert(tk.END, 'No methods are defined in this class\n')
+
     #Adding variables
-    st3=dictClassVariables[className]
-    Section3 = tk.Text(windowNew, height=6, width=30)
-    Section3.pack()
-    for i in st3:
-        Section3.insert(tk.END, i+'\n')
-       
+    Section3 = tk.Text(windowNew, height=10, width=40)
+    Section3.pack(pady = 20)
+    if className in dictClassVariables:
+        Section3.insert(tk.END, 'Format: <Type> <Variable Name>\n\n')
+        st3=dictClassVariables[className]
+        for i in st3:
+            Section3.insert(tk.END, i+'\n')
+    else:
+        Section2.insert(tk.END, 'No variables are defined in this class\n')
+
 
 window = tk.Tk()
 window.title("ReConnect: Class Level Architecture Recovery")
-window.geometry('700x1000')
+window.geometry('500x1000')
 window['bg'] = 'LightBlue1'
 
 for i in range (0, len(classList)):
