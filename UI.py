ba8ffebb6de0fa96f99bb7671f0cff63524ae029
gagannagpal131 <gagannagpal131@gmail.com>
2019-11-21 10:07:59 -0500

    Border added in Main View and Class View

--- UI.py
+++ UI.py
@@ -19,6 +19,9 @@ def printClassDetails(className):
     windowNew = tk.Tk()
     windowNew.title(className)
     heading = tk.Label(windowNew,text="Class: "+className,pady=20,bg='grey1',fg='green1',font=("century gothic", 20,"bold"))
+    windowNew['highlightcolor'] = 'blue'
+    windowNew['highlightbackground'] = 'blue'
+    windowNew['highlightthickness']=20
     heading.pack()
     #windowStart = 500+(counter*300)
     windowStart = 100+randint(100,700)
@@ -83,20 +86,22 @@ window = tk.Tk()
 window.title("ReConnect: Class Level Architecture Recovery")
 window.geometry('600x700')
 window['bg'] = 'Grey1'
+window['highlightcolor'] = 'blue'
+window['highlightbackground'] = 'blue'
+window['highlightthickness']=30
 
 heading = tk.Label(window,text="ReConnect\n Class Level Architecture Recovery ",pady=25,bg='grey1',fg='green1',relief='groove',bd=5,font=("century gothic", 25,"bold"))
-
 heading.pack()
 
 heading1 = tk.Label(window,text="All the classes present in the Input source are given below.\nClick on a class to view its Inheritance, Methods and Variables.",pady=20,bg='grey1',fg='green1',font=("calibri", 14))
 heading1.pack()
 
 for i in range (0, (int(len(classList)/2))):
-    button = tk.Button(window, width=20, text=classList[i],fg='grey1',font=("calibri", 12,"bold"),command=lambda i = i: printClassDetails(classList[i])).place(x=70, y=200+(i*50))
+    button = tk.Button(window, width=20, text=classList[i],fg='grey1',font=("calibri", 12,"bold"),command=lambda i = i: printClassDetails(classList[i])).place(x=50, y=200+(i*50))
     #button.pack(side=tk.TOP,pady=5)
     #button.grid(columnspan=3)
 
 for i in range ((int(len(classList)/2)+1),len(classList)):
-    button1 = tk.Button(window, width=20, text=classList[i],fg='grey1',font=("calibri", 12,"bold"),command=lambda i = i: printClassDetails(classList[i])).place(x=320, y=200+(i-(int(len(classList)/2))-1)*50)
+    button1 = tk.Button(window, width=20, text=classList[i],fg='grey1',font=("calibri", 12,"bold"),command=lambda i = i: printClassDetails(classList[i])).place(x=300, y=200+(i-(int(len(classList)/2))-1)*50)
 
 window.mainloop()
