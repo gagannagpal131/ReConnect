9beb26e4a2621191cbad8baa751070dad0ffb0f2
gagannagpal131 <gagannagpal131@gmail.com>
2019-11-21 13:48:05 -0500

    UI Colour Scheme Updated

--- UI.py
+++ UI.py
@@ -18,22 +18,22 @@ def printClassDetails(className):
     #Creation of Class view
     windowNew = tk.Tk()
     windowNew.title(className)
-    heading = tk.Label(windowNew,text="Class: "+className,pady=20,bg='grey1',fg='green1',font=("century gothic", 20,"bold"))
-    windowNew['highlightcolor'] = 'blue'
-    windowNew['highlightbackground'] = 'blue'
+    heading = tk.Label(windowNew,text="Class: "+className,pady=20,bg='LightBlue2',fg='grey1',font=("century gothic", 20,"bold"))
+    windowNew['highlightcolor'] = 'LightBlue4'
+    windowNew['highlightbackground'] = 'LightBlue4'
     windowNew['highlightthickness']=20
     heading.pack()
     #windowStart = 500+(counter*300)
     windowStart = 100+randint(100,700)
     windowNew.geometry('450x600+%d+%d'%(windowStart,0))
-    windowNew['bg'] = 'grey1'
+    windowNew['bg'] = 'LightBlue2'
 
     #Creating Section in Class View for viewing Inheritences
     #Adding inheritences
     Section1 = tk.Text(windowNew, height=7, width=45)
-    Section1['bg'] = 'grey1'
+    Section1['bg'] = 'wheat1'
     Section1['font'] = "calibri 13 bold"
-    Section1['fg'] = 'green1'
+    Section1['fg'] = 'grey1'
     Section1['bd'] = 4
     Section1.pack(pady = 5)
     # Section1.insert(tk.END, 'Classes Inherited by class '+className+ ' are given below:\n\n')
@@ -51,8 +51,8 @@ def printClassDetails(className):
     #Creating Section in Class View for viewing Methods
     #Adding methods
     Section2 = tk.Text(windowNew, height=7, width=45)
-    Section2['bg'] = 'grey1'
-    Section2['fg'] = 'green1'
+    Section2['bg'] = 'wheat1'
+    Section2['fg'] = 'grey1'
     Section2['bd'] = 4
     Section2['font'] = "calibri 13 bold"
     Section2.pack(pady = 5)
@@ -69,8 +69,8 @@ def printClassDetails(className):
     #Creating Section in Class View for viewing Variables
     #Adding variabless
     Section3 = tk.Text(windowNew, height=7, width=45)
-    Section3['bg'] = 'grey1'
-    Section3['fg'] = 'green1'
+    Section3['bg'] = 'wheat1'
+    Section3['fg'] = 'grey1'
     Section3['bd'] = 4
     Section3['font'] = "calibri 13 bold"
     Section3.pack(pady = 5)
@@ -88,15 +88,15 @@ def printClassDetails(className):
 window = tk.Tk()
 window.title("ReConnect: Class Level Architecture Recovery")
 window.geometry('600x700')
-window['bg'] = 'Grey1'
-window['highlightcolor'] = 'blue'
-window['highlightbackground'] = 'blue'
-window['highlightthickness']=30
+window['bg'] = 'LightBlue2'
+window['highlightcolor'] = 'LightBlue4'
+window['highlightbackground'] = 'LightBlue4'
+window['highlightthickness']=35
 
-heading = tk.Label(window,text="ReConnect\n Class Level Architecture Recovery ",pady=25,bg='grey1',fg='green1',relief='groove',bd=5,font=("century gothic", 25,"bold"))
+heading = tk.Label(window,text="ReConnect\n Class Level Architecture Recovery ",pady=25,bg='LightBlue2',fg='grey1',relief='groove',bd=5,font=("century gothic", 25,"bold"))
 heading.pack()
 
-heading1 = tk.Label(window,text="All the classes present in the Input source are given below.\nClick on a class to view its Inheritance, Methods and Variables.",pady=20,bg='grey1',fg='green1',font=("calibri", 14))
+heading1 = tk.Label(window,text="All the classes present in the Input source are given below.\nClick on a class to view its Inheritance, Methods and Variables.",pady=20,bg='LightBlue2',fg='grey1',font=("calibri", 14))
 heading1.pack()
 
 for i in range (0, (int(len(classList)/2))):
